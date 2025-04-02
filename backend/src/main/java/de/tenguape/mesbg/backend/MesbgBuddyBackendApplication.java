4716aef5a48daeffdf329748961ad167a2112ab7
Dominik Talar <do_ta@web.de>
2025-04-02 18:19:25 +0200

    Add backend, create monorepo

---
+++ backend/src/main/java/de/tenguape/mesbg/backend/MesbgBuddyBackendApplication.java
@@ -0,0 +1,13 @@
+package de.tenguape.mesbg.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesbgBuddyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesbgBuddyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/tenguape/mesbg/backend/MesbgBuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tenguape.mesbg.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesbgBuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
